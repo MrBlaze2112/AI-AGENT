bb3f6833cdc834ecc07011254df3a4381df37760
MrBlaze2112 <akarshsurya31@gmail.com>
2025-11-09 16:01:53 +0530

    Create agents.py

---
+++ agents.py
@@ -0,0 +1,26 @@
+from crewai import Agent
+from tools import web_tool
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+os.environ["OPEN_API_KEY"] = os.getenv("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")
+os.environ["OPENAI_MODEL_NAME"]="gpt-4-0125-preview"
+
+
+## Create a senior blog content researcher
+
+blog_researcher=Agent(
+    role='Movie reviewer from IMDb',
+    goal='get the relevant info for the show or movie {topic} from the provided website',
+    verboe=True,
+    memory=True,
+    backstory=(
+       "Expert in understanding the movie,its hidden subplots,its human touch and rating it properly" 
+    ),
+    tools=[web_tool],
+    allow_delegation=True
+
+)
