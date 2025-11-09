0d5ea8b8552425d821d5958e2dc4f299ce373e98
MrBlaze2112 <akarshsurya31@gmail.com>
2025-11-09 15:43:10 +0530

    tools.py

---
+++ tools.py
@@ -0,0 +1,4 @@
+from crewai_tools import WebsiteSearchTool
+
+# Initialize the tool with a specific Youtube channel handle to target your search
+web_tool = WebsiteSearchTool(website='https://www.espncricinfo.com/')
