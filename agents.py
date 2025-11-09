18f126e56f3621f2a64c637abf1e669f463036c0
MrBlaze2112 <akarshsurya31@gmail.com>
2025-11-09 16:55:01 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -2,25 +2,21 @@ from crewai import Agent
 from tools import web_tool
 
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-import os
-os.environ["OPEN_API_KEY"] = os.getenv("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")
-os.environ["OPENAI_MODEL_NAME"]="gpt-4-0125-preview"
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_MODEL_NAME"] = "gpt-4-0125-preview"
 
-
-## Create a senior blog content researcher
-
-movie_analyst=Agent(
+movie_analyst = Agent(
     role='Movie reviewer from IMDb',
     goal='get the relevant info for the show or movie {topic} from the provided website',
-    verboe=True,
+    verbose=True,  
     memory=True,
     backstory=(
-       "Expert in understanding the movie,its hidden subplots,its human touch and rating it properly" 
+       "Expert in understanding the movie, its hidden subplots, its human touch and rating it properly" 
     ),
     tools=[web_tool],
     allow_delegation=True
-
 )
